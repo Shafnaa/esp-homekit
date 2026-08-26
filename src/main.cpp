d8a56f1d0aa274c2acd14bfb48e34160a724b0e7
Saujana Shafi <hello@saujanashafi.com>
2026-08-26 16:24:53 +0700

    feat(firmware): add WS2812 blink and serial startup verification firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <Arduino.h>
 
-void setup() {}
+static constexpr uint8_t kLedPin = 21;
+static constexpr uint8_t kBrightness = 16;
+static constexpr uint32_t kBlinkHalfPeriodMs = 500;
 
-void loop() {}
+static bool ledOn = false;
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("[esp-homekit] verification firmware booted");
+    Serial.printf("[esp-homekit] built " __DATE__ " " __TIME__ "\n");
+}
+
+void loop() {
+    if (ledOn) {
+        neopixelWrite(kLedPin, kBrightness, 0, 0);
+    } else {
+        neopixelWrite(kLedPin, 0, 0, 0);
+    }
+    ledOn = !ledOn;
+    delay(kBlinkHalfPeriodMs);
+}

7cb986389ba4b114f809e7f06576773096b0dc48
Saujana Shafi <hello@saujanashafi.com>
2026-08-27 15:49:59 +0700

    feat(homespan): add wifi and homespan discoverability at boot

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,17 @@
 #include <HomeSpan.h>
 
 static constexpr uint8_t kLedPin = 21;
-static constexpr uint8_t kBrightness = 16;
-static constexpr uint32_t kBlinkHalfPeriodMs = 500;
 static constexpr uint32_t kStartupDelayMs = 3000;
-
-static bool ledOn = false;
-
-static void setLed(uint8_t red, uint8_t green, uint8_t blue) {
-    rgbLedWrite(kLedPin, red, green, blue);
-}
 
 void setup() {
     Serial.begin(115200);
     delay(kStartupDelayMs);
     Serial.println();
-    Serial.println("[esp-homekit] verification firmware booted");
+    Serial.println("[esp-homekit] firmware booted, starting HomeSpan");
     Serial.printf("[esp-homekit] built " __DATE__ " " __TIME__ "\n");
 
     homeSpan.setPairingCode("11122333");
+    homeSpan.enableAutoStartAP();
     homeSpan.begin(Category::Switches, "esp-homekit switch", "esp-homekit");
 
     new SpanAccessory();
@@ -27,16 +20,10 @@ void setup() {
     new Characteristic::Identify();
 
     new Service::Switch();
+
+    rgbLedWrite(kLedPin, 0, 0, 0);
 }
 
 void loop() {
     homeSpan.poll();
-
-    if (ledOn) {
-        setLed(kBrightness, 0, 0);
-    } else {
-        setLed(0, 0, 0);
-    }
-    ledOn = !ledOn;
-    delay(kBlinkHalfPeriodMs);
 }
